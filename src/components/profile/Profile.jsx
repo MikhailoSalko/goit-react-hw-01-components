c444332797bda9635ad0a8ae29a0f3a9e55dda6d
Mikhailo Salko <slipknotmisha@gmail.com>
2023-04-30 14:47:24 +0300

    add the rest of solution to the first task

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,29 +1,42 @@
 import css from './Profile.module.css';
+import PropTypes from 'prop-types';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div class={css.profile}>
-      <div class="description">
-        <img src={avatar} alt={username} class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">{`@${tag}`}</p>
-        <p class="location">{location}</p>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{`@${tag}`}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul class={css.stats}>
-        <li className={css.stats}>
-          <span class="label">Followers</span>
-          <span class="quantity">{stats.followers}</span>
+      <ul className={css.stats}>
+        <li className={css.item}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
-        <li className={css.stats}>
-          <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
+        <li className={css.item}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
-        <li className={css.stats}>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
+        <li className={css.item}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.exact({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
 };
