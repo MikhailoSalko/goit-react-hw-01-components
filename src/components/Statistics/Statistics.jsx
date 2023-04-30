12d87f9b09c18a7dbb5cc073f78de3f060bceb61
Mikhailo Salko <slipknotmisha@gmail.com>
2023-04-30 16:08:37 +0300

    add solution for the 2 task

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,47 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>Upload stats</h2>}
+
+      <ul className={css.list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li className={css.item} key={id}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{`${percentage}%`}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      labe: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+};
+
+// <li className={css.item} key={id}>
+//   <span className={css.label}>{label}</span>
+//   <span className={css.percentage}>{`${percentage}%`}</span>
+// </li>
+// <li className={css.item} key={id}>
+//   <span className={css.label}>{label}</span>
+//   <span className={css.percentage}>{`${percentage}%`}</span>
+// </li>
+// <li className={css.item} key={id}>
+//   <span className={css.label}>{label}</span>
+//   <span className={css.percentage}>{`${percentage}%`}</span>
+// </li>
+// <li className={css.item} key={id}>
+//   <span className={css.label}>{label}</span>
+//   <span className={css.percentage}>{`${percentage}%`}</span>
+// </li>
