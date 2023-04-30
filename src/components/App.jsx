4016a1f329bfef06e32aa8c6c655e168f8a8a1bf
Mikhailo Salko <slipknotmisha@gmail.com>
2023-04-30 11:02:06 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Profile } from 'components/profile/Profile';
+import user from 'components/user.json';
 
 export const App = () => {
   return (
@@ -12,7 +13,13 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,29 +1,27 @@
-export const Profile = props => {
+import css from './Profile.module.css';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div class="profile">
+    <div class={css.profile}>
       <div class="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+        <img src={avatar} alt={username} class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">{`@${tag}`}</p>
+        <p class="location">{location}</p>
       </div>
 
-      <ul class="stats">
-        <li>
+      <ul class={css.stats}>
+        <li className={css.stats-}>
           <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span class="quantity">{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.stats-}>
           <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span class="quantity">{stats.views}</span>
         </li>
-        <li>
+        <li className={css.stats-}>
           <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span class="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
